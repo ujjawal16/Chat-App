40750dec5e08cf0d74ad08c1212be12667c9b957
ujjawal16 <ujjawal.golani@gmail.com>
2021-07-25 01:39:59 +0530

    public and private routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss'
+import {Switch} from 'react-router'
+import SignIn from './pages/SignIn';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
-  return (
-    <div className="App">
-     Hello
-    </div>
-  );
+  return(
+  <Switch>
+    <PublicRoute>
+      <SignIn  path='/signin'/>
+    </PublicRoute>
+    <PrivateRoute path ='/' >
+      <Home />
+    </PrivateRoute>
+  </Switch>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {BrowserRouter} from 'react-router-dom' 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />,
+
+ReactDOM.render(
+<BrowserRouter>
+<App />
+</BrowserRouter>,
   document.getElementById('root')
 );
 
