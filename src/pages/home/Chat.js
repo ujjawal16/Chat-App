9f7d05c6bc380018758acb6860ce3fd93297b6f3
ujjawal16 <ujjawal.golani@gmail.com>
2021-07-27 10:41:25 +0530

    Chat Layout Creation

---
+++ src/pages/home/Chat.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useParams } from 'react-router'
+import { Loader } from 'rsuite'
+import Bottom from '../../components/chat-window/bottom'
+import Messages from '../../components/chat-window/messages'
+import Top from '../../components/chat-window/top'
+import { useRooms } from '../../context/rooms.context'
+
+const Chat = () => {
+
+    const {chatId}=useParams()
+
+    const rooms=useRooms()
+
+    if(!rooms){
+        return <Loader center vertical size="md" content="Loading"  speed="slow" />
+    }  
+
+    const currRoom=rooms.find(room=>room.id===chatId)
+
+    if(!currRoom)
+    {
+        return <h6 className="text-center mt-page">Chat {chatId} not Found</h6>
+    }
+    return (
+        <>
+
+           <div className="chat-top">
+            <Top />
+           </div>
+
+           <div className="chat-middle">
+            <Messages />
+           </div>
+
+           <div className="chat-bottom">
+            <Bottom />
+           </div>
+
+        </>
+    )
+}
+
+export default Chat
