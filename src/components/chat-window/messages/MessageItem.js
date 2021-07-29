96586b64cecf92e08ff043856b9e4e228c10e320
ujjawal16 <ujjawal.golani@gmail.com>
2021-07-29 10:59:58 +0530

    chat likes and deletion

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -2,24 +2,34 @@ import React,{memo} from 'react'
 import { Button } from 'rsuite'
 import TimeAgo from 'timeago-react'
 import { useCurrentRoom } from '../../../context/currentRoom.context'
+import { useHover, useMediaQuery } from '../../../misc/CustomHooks'
 import { auth } from '../../../misc/firebase'
 import ProfileAvatar from '../../dashboard/ProfileAvatar'
 import Presence from '../../Presence'
+import IconBtnControl from './IconBtnControl'
 import ProfileInfoBtnModal from './ProfileInfoBtnModal'
 
 
-const MessageItem = ({message,handleAdmin}) => {
+const MessageItem = ({message,handleAdmin,handleLike,handleDelete}) => {
 
-    const {author,createdAt,text}=message
+    const {author,createdAt,text,likes,likeCount}=message
+    const isMobile=useMediaQuery(('(max-width:992px)'))
+
+
+    const [selfRef,isHover]=useHover()
+
     const isAdmin=useCurrentRoom(v=>v.isAdmin)
     const admins=useCurrentRoom(v=>v.admins)
 
     const isMsgAuthorAdmin=admins.includes(author.uid)
     const isAuthor=auth.currentUser.uid===author.uid
     const canGrantAdmin=isAdmin||!isAuthor
+    const canShowIcons=isMobile||isHover
+
+    const isLiked=likes&& Object.keys(likes).includes(auth.currentUser.uid)
 
     return (
-        <li className="padded mb-1">
+        <li className={`padded mb-1 cursor-pointer ${isHover?'bg-black-02':' '}`} ref={selfRef}>
             <div className="d-flex alighn-items-center font-bolder mb-1">
                 <Presence uid={author.uid} />
                 <ProfileAvatar src={author.avatar} name={author.name} className="ml-1" size="xs" />
@@ -33,6 +43,22 @@ const MessageItem = ({message,handleAdmin}) => {
                     }
                 </ProfileInfoBtnModal >
                 <TimeAgo datetime={createdAt} className="font-normal text-black-45 ml-2" />
+
+                <IconBtnControl
+                {...(isLiked?{color:'red'}:{})}
+                isVisible={canShowIcons}
+                iconName="heart"
+                tooltip="Like this message"
+                onClick={()=>{handleLike(message.id)}}
+                badgeContent={likeCount} />
+
+                {isAuthor&&
+                 <IconBtnControl
+                 isVisible={canShowIcons}
+                 iconName="close"
+                 tooltip="Delete this message"
+                 onClick={()=>{handleDelete(message.id)}}
+                />}
             </div>
 
             <div>
--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import { Alert } from 'rsuite'
-import { database } from '../../../misc/firebase'
+import { auth, database } from '../../../misc/firebase'
 import { transformToArrWithId } from '../../../misc/helper'
 import MessageItem from './MessageItem'
 
@@ -47,11 +47,79 @@ const Messages = () => {
                 Alert.info(alertMsg,4000)
             }, [chatId]
     )
+
+    const handleLike=useCallback(async(msgId) => {
+        const {uid}=auth.currentUser
+        const messageRef=database.ref(`/messages/${msgId}`)
+        
+        let alertMsg
+        
+        await messageRef.transaction(msg=>{
+            if (msg) {
+                if (msg.likes && msg.likes[uid]) {
+                    msg.likeCount-=1;
+                    msg.likes[uid]=null
+                    alertMsg='Like Removed'
+                  
+                } else {
+                    msg.likeCount+=1;
+
+                    if(!msg.likes)
+                    {
+                        msg.likes={}
+                    }
+
+                    msg.likes[uid]=true
+                    alertMsg='Message Liked'
+                }
+              }
+              return msg;
+            });
+            Alert.info(alertMsg,4000)
+            
+        }, [],
+    )
+
+    const handleDelete=useCallback(async(msgId) => {
+        // eslint-disable-next-line no-alert
+        if(!window.confirm('Delete this Message?')){
+            return;
+        }
+
+        const isLast=messages[messages.length-1].id===msgId
+        const updates={}
+        updates[`/messages/${msgId}`]=null
+
+        if(isLast&&messages.length>1)
+        {
+            updates[`/rooms/${chatId}/lastMessage`]={
+                ...messages[messages.length-2],
+                msgId:messages[messages.length-2].id
+            }
+        }
+
+        if(isLast&&messages.length===1){
+            updates[`/rooms/${chatId}/lastMessage`]=null
+        }
+
+        try {
+            await database.ref().update(updates)
+            Alert.info('Message has been deleted',4000)
+        } catch (error) {
+            Alert.error(error.message,4000)
+        }
+
+           
+        },
+        [chatId,messages],
+    )
     return (
         <ul className="msg-list custom-scroll">
             {isChatEmpty&&<li>No Messages Yet!</li>}
             {canShowMessages &&
-            messages.map(msg=><MessageItem key={msg.id} message={msg} handleAdmin={handleAdmin} />)
+            messages.map(msg=><MessageItem key={msg.id} message={msg} handleAdmin={handleAdmin}
+                 handleLike={handleLike} handleDelete={handleDelete}
+                 />)
             }
         </ul>
     )
