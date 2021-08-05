8c757c077b20eb84af9d2c4b08ac730159b7e748
ujjawal16 <ujjawal.golani@gmail.com>
2021-08-05 10:25:22 +0530

    added signin lazy loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React,{lazy, Suspense} from 'react';
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss'
 import {Switch} from 'react-router'
-import SignIn from './pages/SignIn';
+// import SignIn from './pages/SignIn';
 import PrivateRoute from './components/PrivateRoute';
 import Home from './pages/home/index';
 import PublicRoute from './components/PublicRoute';
 import { ProfileProvider } from './context/profile.context';
 import { ErrorBoundary } from './components/ErrorBoundary';
+
+const SignIn = lazy(() => import('./pages/SignIn'));
 
 function App() {
   return(
@@ -15,7 +17,9 @@ function App() {
   <ProfileProvider>
       <Switch>
         <PublicRoute path ='/signin'>
+        <Suspense fallback={<div>Loading...</div>}>
           <SignIn  path='/signin'/>
+          </Suspense>
         </PublicRoute>
         <PrivateRoute path ='/' >
           <Home />
